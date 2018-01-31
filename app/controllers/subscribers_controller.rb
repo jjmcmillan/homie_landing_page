034356071e350164395b2ce0e5af927e53a55d38
Jack McMillan <jj.mcmillan.97@gmail.com>
2018-01-31 19:51:38 +0000

    Added database for emails

---
+++ app/controllers/subscribers_controller.rb
@@ -0,0 +1,19 @@
+class SubscribersController < ApplicationController
+  def new
+    @subscriber = Subscriber.new
+  end
+  
+  def create
+    @subscriber = Subscriber.new(subscriber_params)
+    if @subscriber.save
+       redirect_to new_subscriber_path, notice: "Message sent."
+    else
+       redirect_to new_subscriber_path, notice: "Error occured."
+    end
+  end
+  
+  private
+    def subscriber_params
+       params.require(:subscriber).permit(:email)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'subscribers#new'
+  resources :subscribers
 end
